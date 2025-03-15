4a0715b1857c1aa32b42c11bfe388c7921586eec
Sarvesh1709 <sarveshdlatkar@gmail.com>
2025-03-15 16:28:56 +0530

    sample commit

---
+++ src/test/java/BrowserTestJava.java
@@ -0,0 +1,21 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BrowserTestJava {
+  public static void main(String[] args) {
+	  String projectPath =System.getProperty("user.dir");
+	  System.out.println("projectPath :"+projectPath);
+	  
+	  System.setProperty("webdriver.chrome.driver", projectPath+"\\drivers\\chromedriver\\chromedriver.exe");
+	  WebDriver driver=new ChromeDriver();
+	  
+		// driver.get("http://www.google.com");
+		 driver.findElement(By.id("lst-ib")).sendKeys("abcd");
+		 WebElement textBox= driver.findElement(By.id("lst-ib"));
+		 textBox.sendKeys("Automation Step by Step");
+		 driver.close();
+	//	 driver.quit();
+}
+}
